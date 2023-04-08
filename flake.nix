5cdb444727e5092a32558317c3d2f085c29f360b
Break Yang <breakds@gmail.com>
2023-04-07 17:50:57 -0700

    remove libstdc++ in LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -62,8 +62,6 @@
         shellHook = ''
           export CC=clang
           export CXX=clang++
-          # Manually set where to look for libstdc++.so.6
-          export LD_LIBRARY_PATH=${pkgs-dev.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
           export PS1="$(echo -e '\uf1c0') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
          '';
       };
